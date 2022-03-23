c93feaf336318c4194eb2858d1268003d374072d
ilaydadora <100840400+ilaydadora@users.noreply.github.com>
2022-03-23 15:14:16 +0300

    TimerProject
    Basic Timer

---
+++ TimerProject/TimerProject/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  TimerProject
+//
+//  Created by İlayda Metin on 6.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var timer = Timer()
+    var counter = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        counter = 10
+        timeLabel.text = "Time: \(counter)"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func timerFunction() {
+        
+        timeLabel.text = "Time: \(counter)"
+        counter -= 1
+        
+        if counter == 0 {
+            timer.invalidate()
+            timeLabel.text = "Time's Over"
+        }
+        
+    }
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        print("button clicked")
+    }
+    
+}
+
---
+++ TimerProject/TimerProjectUITests/TimerProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TimerProjectUITestsLaunchTests.swift
+//  TimerProjectUITests
+//
+//  Created by İlayda Metin on 6.03.2022.
+//
+
+import XCTest
+
+class TimerProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
